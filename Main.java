31b11359903a687a28087b93868df00b46ed17ca
suguru-3u <suguru.13579515@gmail.com>
2020-11-10 17:22:25 +0900

    study Java

---
+++ Main.java
@@ -0,0 +1,14 @@
+/*
+/@Java‚ÌŠwK
+/  ŠwK“ú2020/11/10
+*/
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        final String japanString = "‚±‚ñ‚É‚¿‚í";
+        System.out.println(japanString);
+        String japanLangage = "aaa";
+        System.out.println(japanLangage);
+    }
+}
