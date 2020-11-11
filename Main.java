95531f553b4aeba38d36838fd3acbd5cf7c4f5a7
suguru-3u <suguru.13579515@gmail.com>
2020-11-11 11:17:29 +0900

    todaay study java

--- Main.java
+++ Main.java
@@ -14,5 +14,16 @@ public class Main{
         int b = 5;
         int c = a + b;
         System.out.println(c);
+        int age = (int)3.2;
+        System.out.println(age);
+        String msg = "Ž„‚Ì–¼‘O‚Í" + 23 ;
+        System.out.println(msg);
+        int m = Math.max(a,b);
+        System.out.println(m);
+        int r = new java.util.Random().nextInt(100);
+        System.out.println(r);
+        System.out.println("‚ ‚È‚½‚Ì–¼‘O‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢");
+        String name = new java.util.Scanner(System.in).nextLine();
+        System.out.println(name);
     }
 }
