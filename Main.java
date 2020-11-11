dc0ed840d6656909d91ec3c759c1eb9043e4fb43
suguru-3u <suguru.13579515@gmail.com>
2020-11-11 11:27:53 +0900

    todaay study java

--- Main.java
+++ Main.java
@@ -5,25 +5,38 @@
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello world");
-        final String japanString = "こんにちわ！";
-        System.out.println(japanString);
-        String japanLangage = "aaa";
-        System.out.println(japanLangage);
-        int a = 4;
-        int b = 5;
-        int c = a + b;
-        System.out.println(c);
-        int age = (int)3.2;
-        System.out.println(age);
-        String msg = "私の名前は" + 23 ;
-        System.out.println(msg);
-        int m = Math.max(a,b);
-        System.out.println(m);
-        int r = new java.util.Random().nextInt(100);
-        System.out.println(r);
+        // System.out.println("Hello world");
+        // final String japanString = "こんにちわ！";
+        // System.out.println(japanString);
+        // String japanLangage = "aaa";
+        // System.out.println(japanLangage);
+        // int a = 4;
+        // int b = 5;
+        // int c = a + b;
+        // System.out.println(c);
+        // int age = (int)3.2;
+        // System.out.println(age);
+        // String msg = "私の名前は" + 23 ;
+        // System.out.println(msg);
+        // int m = Math.max(a,b);
+        // System.out.println(m);
+        // int r = new java.util.Random().nextInt(100);
+        // System.out.println(r);
+        // System.out.println("あなたの名前を入力してください");
+        // String name = new java.util.Scanner(System.in).nextLine();
+        // System.out.println(name);
+        
+        // 2章の課題
+        System.out.println("ようこそ占いの館へ！！！");
         System.out.println("あなたの名前を入力してください");
         String name = new java.util.Scanner(System.in).nextLine();
-        System.out.println(name);
+        System.out.println("あなたの年齢を入力してください");
+        String ageString = new java.util.Scanner(System.in).nextLine();
+        int age = Integer.parseInt(ageString);
+        int fortune = new java.util.Random().nextInt(4);
+        fortune++;
+        System.out.println("占いの結果が出ました！");
+        System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+        System.out.println("１：大吉　２：中吉　３：吉　４：凶");
     }
 }
