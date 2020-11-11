ab3d5efe308443067800667ebc1f0f329c884ac7
suguru-3u <suguru.13579515@gmail.com>
2020-11-11 10:58:59 +0900

    todaay study java

--- Main.java
+++ Main.java
@@ -1,14 +1,18 @@
 /*
-/　Javaの学習
-/  学習日2020/11/10
+/?@Java??w?K
+/  ?w?K??2020/11/10
 */
 
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
-        final String japanString = "こんにちわ";
+        final String japanString = "こんにちわ！";
         System.out.println(japanString);
         String japanLangage = "aaa";
         System.out.println(japanLangage);
+        int a = 4;
+        int b = 5;
+        int c = a + b;
+        System.out.println(c);
     }
 }
