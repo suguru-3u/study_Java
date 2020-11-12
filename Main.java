bf93d2de762b26ecd2998a60924f4a0420de28ae
suguru-3u <suguru.13579515@gmail.com>
2020-11-13 07:40:22 +0900

    Today java study

--- Main.java
+++ Main.java
@@ -27,16 +27,24 @@ public class Main{
         // System.out.println(name);
         
         // 2章の課題
-        System.out.println("ようこそ占いの館へ！！！");
-        System.out.println("あなたの名前を入力してください");
-        String name = new java.util.Scanner(System.in).nextLine();
-        System.out.println("あなたの年齢を入力してください");
-        String ageString = new java.util.Scanner(System.in).nextLine();
-        int age = Integer.parseInt(ageString);
-        int fortune = new java.util.Random().nextInt(4);
-        fortune++;
-        System.out.println("占いの結果が出ました！");
-        System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
-        System.out.println("１：大吉　２：中吉　３：吉　４：凶");
+        // System.out.println("ようこそ占いの館へ！！！");
+        // System.out.println("あなたの名前を入力してください");
+        // String name = new java.util.Scanner(System.in).nextLine();
+        // System.out.println("あなたの年齢を入力してください");
+        // String ageString = new java.util.Scanner(System.in).nextLine();
+        // int age = Integer.parseInt(ageString);
+        // int fortune = new java.util.Random().nextInt(4);
+        // fortune++;
+        // System.out.println("占いの結果が出ました！");
+        // System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+        // System.out.println("１：大吉　２：中吉　３：吉　４：凶");
+
+        // 3章スタート
+        boolean tenki = true;
+        if(tenki){
+            System.out.println("選択します");
+        }else{
+            System.out.println("DVDをみます");
+        }
     }
 }
